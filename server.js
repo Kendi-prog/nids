c2edaf75fe0e1118b4a21214673b22b72d65807e
Kendi <muriungikendileila@gmail.com>
2025-04-08 13:11:05 +0300

    connecting mongodb

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-// const connectDB = require('./config/db'); 
+const bodyParser = require('body-parser'); 
 const userRoutes = require('./routes/userRoutes'); 
 const alertRoutes = require('./routes/alertRoutes');
 const logRoutes = require('./routes/logRoutes');
@@ -12,7 +13,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-// connectDB();
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+  
 
 // Middleware
 app.use(cors()); 
